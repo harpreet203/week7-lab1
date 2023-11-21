1c5c71385c5246fe09ef98c2be93b41aecf33f42
Harpreet Singh <naharharpreet1988@gmail.com>
2023-11-21 20:58:54 +0200

    Lab-1 done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is <code>Harpreet's Screen</code> and Lab 1.
         </p>
         <a
           className="App-link"
